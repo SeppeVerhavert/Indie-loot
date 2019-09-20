90f36b493c5e5d25e6bf460f880c43408ac0f27c
seppe_verhavert <seppe.verhavert@gmail.com>
2019-09-20 11:44:57 +0200

    add CR slider

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,22 +1,41 @@
 let amountSlider = document.getElementById('amountRange');
 let amountText = document.getElementById('amountText');
 amountSlider.addEventListener('input', calculateAmount);
-let amountArray = [ 'No treasure (or angry DM)',
-                    'Pocket change',
-                    'Even fewer treasure',
-                    'Small amount of treasure',
-                    'Slightly less treasure',
-                    'Medium amount of treasure',
-                    'Slightly more treasure',
-                    'Quite some treasure',
-                    'A lot of treasure',
-                    'Insane amount of treasure',
-                    'Treasure Hoard'];
+let amountArray = [
+    'No treasure (or angry DM)',
+    'Pocket change',
+    'Even fewer treasure',
+    'Small amount of treasure',
+    'Slightly less treasure',
+    'Medium amount of treasure',
+    'Slightly more treasure',
+    'Quite some treasure',
+    'A lot of treasure',
+    'Insane amount of treasure',
+    'Treasure Hoard'];
 
-// document.getElementById('generateBtn').addEventListener('click', myFunction);
-// let text = document.getElementById('treasuretext');
+let crSlider = document.getElementById('crRange');
+let crText = document.getElementById('crText');
+crSlider.addEventListener('input', calculateCr);
+let crArray = [
+    '0 - 4',
+    '5 - 10',
+    '11 - 16',
+    '17+'];
 
-function calculateAmount(){
+document.getElementById('generateBtn').addEventListener('click', generateTreasure);
+let treasureText = document.getElementById('treasureText');
+
+function calculateAmount() {
     let range = document.getElementById("amountRange").value;
     amountText.innerHTML = amountArray[range];
+}
+
+function calculateCr() {
+    let range = document.getElementById("crRange").value;
+    crText.innerHTML = "Challenge Rating " + crArray[range];
+}
+
+function generateTreasure() {
+
 }
